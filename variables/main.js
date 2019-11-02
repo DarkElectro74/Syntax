5681c1b99e0876f71cd6ce94c9a1926159822008
Egor Blinov <electrodark74@gmail.com>
2019-11-02 11:58:36 +0500

    first commit variables

---
+++ variables/main.js
@@ -0,0 +1,16 @@
+//1 задача
+let algebra = 5;
+let geography = 4;
+let physics = 4;
+average = (algebra + geography + physics) / 3 
+console.log(average);
+//2 задача
+let myName = 'Heisenberg'; //:D
+message = "Привет, мир! Меня зовут "+ myName;
+console.log(message);
+// 3 задача
+let x = 2;
+let y = 22;
+let z = 0;
+result = (x * y + 5 * z + x - 1);
+console.log(result);
