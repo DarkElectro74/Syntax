39da27e14e103fe7b7492a5f5ca5c00b71c01921
Egor Blinov <electrodark74@gmail.com>
2019-11-08 10:17:04 +0500

    Коммит второго домашнего задания

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -1,40 +1,66 @@
-
-
+"use strict";
+//1 задача
 function calculateQuadraticEquation(){
-    let a = +window.a.value;
-    let b = +window.b.value;
-    let c = +window.c.value;
-    let result = getResult(a,b,c);
-    window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
-    let span = window.result;
-    span.textContent = "х = "+result;
-}
+  let a = +window.a.value;
+  let b = +window.b.value;
+  let c = +window.c.value;
+  let result = getResult(a,b,c);
+  window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+  let span = window.result;
+  span.textContent = "х = " + result;
+};
 
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    //return x;
-}
-
+  let x;
+  let d = Math.pow(b, 2) - 4 * a * c;
+      
+  if (d < 0){
+    console.log('Корней нет');
+  } else if (d === 0){
+    x = (-b) / (2 * a);
+    console.log(x);
+    return x;
+  } else {
+    let x1, x2;  
+    x1 = (-b + Math.sqrt(d)) / (2 * a); 
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    console.log('Первый корень' + x1 + ' Второй корень' + x2);
+    return [x1, x2];
+  };
+};
+// 2 задача
 function calculateAverageRating(){
     let marks = window.marks.value.split("").map(Number).filter((n)=> !isNaN(n) && n > 0);
     let averageMark = getAverageMark(marks);
     window.averageMark.textContent = averageMark;
-}
+};
 
 function getAverageMark(marks){
-    // код для задачи №2 писать здесь
-    //return averageMark;
-}
+  let sum = 0;
+  let averageMark = 0;
 
+  if (marks.length > 5){
+    console.log('Количество оценок больше 5');
+    marks.splice(5);
+  };
+
+  for (let i = 0; i < marks.length; i ++){
+  sum = sum + marks[i];  
+  };
+  averageMark = sum / marks.length;
+  return averageMark;
+};
+//3 задача
 function calculateDrinkTask(){
     let name = window.personName.value;
     let dateOfBirthday = new Date(window.dateOfBirthday.value);
     let drink = askDrink(name, dateOfBirthday);
     window.drink.textContent = drink;
-}
+};
 
 function askDrink(name,dateOfBirthday){
-    // код для задачи №3 писать здесь
-    //console.log(result)
-    //return result;
-}
+	let yearOfBirth = dateOfBirthday.getFullYear();
+	let result = ((new Date().getFullYear() - yearOfBirth) >= 18) ? `Не желаете ли олд-фэшн, ${name}?` : `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
+	console.log(result);
+	return result;
+};
