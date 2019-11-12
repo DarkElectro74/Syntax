277e4422df375abe85326fa8efa91700bb8e7ca7
Egor Blinov <electrodark74@gmail.com>
2019-11-12 13:26:07 +0500

    Обновление дз

--- Extended-Syntax/task.js
+++ Extended-Syntax/task.js
@@ -12,17 +12,16 @@ function calculateQuadraticEquation(){
 
 function getResult(a,b,c){
     let d = Math.pow(b,2) - 4 * a * c;
-    if (d < 0){
-        let x = [];
-        return x; 
-    } else if(d == 0){
-        let x = - b / (2 *a);
-        return x;
-    } else {
-        let x = (- b + Math.sqrt(d))/ (2 *a);
+	let x = [];
+	 if(d === 0){
+        let x3 = - b / (2 *a);
+		x.push(x3);
+    } else if (d > 0){
+        let x1 = (- b + Math.sqrt(d))/ (2 *a);
         let x2 = (- b - Math.sqrt(d))/ (2 *a);
-        return [x, ` x2= ${x2}`];   
+		x.push(x1,x2); 
     }
+	return x;
 }
 // 2 задача
 function calculateAverageRating(){
@@ -33,7 +32,6 @@ function calculateAverageRating(){
 
 function getAverageMark(marks){
   let sum = 0;
-  let averageMark = 0;
 
   if (marks.length > 5){
     console.log('Количество оценок больше 5');
