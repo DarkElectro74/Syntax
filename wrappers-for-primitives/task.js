03d21e0059808903aa2ce691310e498fdc9eec16
Egor Blinov <electrodark74@gmail.com>
2019-11-14 10:44:25 +0500

    Коммит дз обертки

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,28 +1,43 @@
-function calculateMortgage() {
-    let percent = window.percent.value;
-    let contribution = window.contribution.value;
-    let amount = window.amount.value;
-    let date = window.date.value;
+"use strict";
 
-    let result = calculateTotalMortgage(percent, contribution, amount, date);
-    let span = window.mortageResult;
-    span.textContent = result;
+function calculateMortgage() {
+  let percent = window.percent.value;
+  let contribution = window.contribution.value;
+  let amount = window.amount.value;
+  let date = window.date.value;
+  let result = calculateTotalMortgage(percent, contribution, amount, date);
+  let span = window.mortageResult;
+  span.textContent = result;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-
-    // код для задачи №1 писать здесь
-    //return totalAmount;
+  let todayDate = new Date();
+  let data = new Date(date);
+  let month = new Date((data.getFullYear() - todayDate.getFullYear()) * 12 + (data.getMonth() + 1) - (todayDate.getMonth() + 1));
+  let monthlyInterestRate = (percent / 12) / 100;
+  let monthlyFee = amount * (monthlyInterestRate + monthlyInterestRate / ((Math.pow((1 + monthlyInterestRate), month) - 1)));
+  let totalAmount = monthlyFee * month + amount - contribution;
+  console.log(totalAmount.toFixed(2));
+  return totalAmount.toFixed(2);
 }
 
+
+
+
+
 function sayHello() {
-    let name = window.personName.value;
-    let greeting = getGreeting(name);
-    let span = window.helloResult;
-    span.textContent = greeting;
+  let name = window.personName.value;
+  let greeting = getGreeting(name);
+  let span = window.helloResult;
+  span.textContent = greeting;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    //return greeting;
+  let user = new String(name);
+  if (name === "" || name === null || name === undefined) {
+    user = 'Аноним';
+  } else {
+    console.log('Привет, мир! Меня зовут ' + name);
+  }
+  return `Привет, мир! Меня зовут ${user}`;//Haisenberg :D
 }
