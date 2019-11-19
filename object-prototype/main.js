287f8f3624a64a8105a8ccc2b73f3c0c84be3e99
Egor Blinov <electrodark74@gmail.com>
2019-11-19 12:00:39 +0500

    Повторный коммит дз прототип

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,3 +1,4 @@
+//3
 function initCheckBirthday() {
     const birthday = document.getElementById('birthday').value;
 
@@ -5,42 +6,42 @@ function initCheckBirthday() {
 
     document.getElementById('disclaimer').innerHTML = result;   
 }
-// 3 задача!!
+
 function checkBirthday(birthday) {
-     let now = new Date();
-	let birth = new Date (birthday);
-	let leapYears = 0;
+ let now = new Date();
+ let birth = new Date (birthday);
+ let leapYears = 0;
 
    for (let i = birth.getFullYear();i <= now.getFullYear();i++){
     if ((i % 4 === 0) && (i % 100 > 0) || (i % 400 === 0)){
    leapYears++;
-	}
+}
 }
     let dif = now - birth;
     let age = Math.floor(dif / (1000 * 60 * 60 * 24 * 365 + leapYears));
     return age >= 18;
 }
-
+//1
 function initPrintAnimalSound() {
     const animal = {
         sound: 'grrrr',
     };
-выф
+
     const result = getAnimalSound(animal);
 
     document.getElementById('sound').innerHTML = result;   
 }
-  // код для задачи №2 писать здесь
-	// 1 задача !!
+
 function getAnimalSound(animal) {
    let sound = animal.sound;
 
     if (animal === undefined) {
-        return: null;
+        return null;
     } else {
-       return: sound;
+       return sound;
     }
 }
+//2
 
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
@@ -51,7 +52,7 @@ function initCalculateStatement() {
         document.getElementById('learner-' + idx + '-average').innerHTML = average;
     }
 }
-// 2 задача!// код для задачи №3 писать здесь
+
 function getAverageMark(marks) {
     let result = 0;
     for (let i = 0;i < marks.length;i++ ){
