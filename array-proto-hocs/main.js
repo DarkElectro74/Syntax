c237023b49a58c8afdbf5e5ad9f8eaf4527532b9
Egor Blinov <electrodark74@gmail.com>
2019-11-22 10:59:50 +0500

    Коммит домашки

---
+++ array-proto-hocs/main.js
@@ -0,0 +1,46 @@
+function compareArrays(arr1, arr2){
+    if (arr1.length === arr2.length){
+        return arr1.every((item, index) => item === arr2[index]);
+    }else{
+        return false;
+    };    
+};       
+
+function memoize(sum, limit){
+    let results = {
+        args : [], 
+        result : []
+    };
+
+    let arrayOfResults = [];
+
+    function fn(a, b){
+        if (arrayOfResults.length === 0){
+            results.args =  [a, b];
+            results.result = sum(a, b);
+            arrayOfResults.push(results);
+            return console.log(results.result);
+        }else{    
+            if (compareArrays(results.args, [a, b])){
+                return console.log('Результат берется из памяти \n' + results.result);
+            }else{ 
+                results.args =  [a, b];
+                results.result = sum(a, b);
+                arrayOfResults.push(results);
+                return console.log('Функция вызвана не из памяти\n' + results.result); 
+            };
+        };    
+    };
+
+    return fn;
+};
+
+const sumfn = (a, b) => a + b;
+
+sumfn (3, 4)
+const mSum = memoize(sumfn, 10)
+
+mSum(3, 4)
+mSum(4, 5)
+mSum(3, 4)
+mSum(2, 4)
